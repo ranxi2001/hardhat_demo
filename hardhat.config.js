3bee7b8add4a5e544167d210f42c078220fd0334
ranxi2001 <ranxi169@163.com>
2024-10-11 22:44:49 +0800

    update 更新完整版hardhat配置文件

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();  // 加载 .env 文件中的变量
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: false
+  }
 };
